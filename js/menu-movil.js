bf562bb76db087eeea0aa28c2616888fa4e7d8ac
Hugo Vidal <hugovima09@gmail.com>
2026-02-23 20:43:12 +0100

    english and git

--- js/menu-movil.js
+++
@@ -1,6 +0,0 @@
-const boton = document.getElementById("menu-bot");
-const menu = document.getElementById("bar");
-
-boton.addEventListener("click", () => {
-  menu.classList.toggle("menu-active"); 
-});
